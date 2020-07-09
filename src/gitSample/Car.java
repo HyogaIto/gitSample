806f77aac32e9fa722e0f408b4b5036359186d98
HyogaIto <hyoga.ito@rakus.co.jp>
2020-07-09 12:24:15 +0900

    first commit

---
+++ src/gitSample/Car.java
@@ -0,0 +1,33 @@
+package gitSample;
+
+public class Car {
+
+	private Integer id;
+	private String name;
+	private Integer speed;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+
+}
